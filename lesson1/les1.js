b54499fe2936793ca19b56b1f1c56554b0905700
OleksandrD <domashalex0589@gmail.com>
2023-11-20 21:35:15 +0100

    commit

---
+++ lesson1/les1.js
@@ -0,0 +1,46 @@
+let a='hello'
+console.log(a)
+let b='owu'
+console.log(b);
+let c='com'
+console.log(c);
+let d='ua'
+console.log(d);
+let e=1
+console.log(1);
+let f=10
+console.log(10);
+let g=-999
+console.log(g);
+let h=123
+console.log(h);
+let i=3.14
+console.log(i);
+let j=2.7
+console.log(j);
+let k=16
+console.log(k);
+let l=true
+console.log(l);
+let m=false
+console.log(m);
+
+let firstName='Oleksandr'
+let middleName='Mykhailovych'
+let lastName='Domash'
+let person=firstName+' '+middleName+' '+lastName
+console.log(person);
+
+let n=100
+console.log(typeof n);
+let o='100'
+console.log(typeof o);
+let p=true
+console.log(typeof true);
+
+let r=prompt('імя')
+console.log(r);
+let s=prompt('по-батькові')
+console.log(s);
+let t =prompt('вік')
+console.log(t);
